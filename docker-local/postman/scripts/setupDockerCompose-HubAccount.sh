e1d499f186cac0e23440a409857371f5a568ab64
Lewis Daly <lewisdaly@me.com>
2020-05-20 11:01:14 +0800

    cleaning up postman runner scripts

--- docker-local/postman/scripts/setupDockerCompose-HubAccount.sh
+++ docker-local/postman/scripts/setupDockerCompose-HubAccount.sh
@@ -2,9 +2,10 @@
 
 PATH=$(npm bin):$PATH
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+POSTMAN_DIR="${DIR}/.."
 
 newman run \
   --delay-request=2000 \
   --folder='Hub Account' \
-  --environment=${DIR}/../environments/Mojaloop-Local-Docker-Compose.postman_environment.json \
-  ${DIR}/../OSS-New-Deployment-FSP-Setup.postman_collection.json
+  --environment=${POSTMAN_DIR}/environments/Mojaloop-Local-Docker-Compose.postman_environment.json \
+  ${POSTMAN_DIR}/OSS-New-Deployment-FSP-Setup.postman_collection.json
